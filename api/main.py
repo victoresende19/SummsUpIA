982b1fd8e3240faadbcf0596a0457face34c55cc
Victor Resende <victoresende19@gmail.com>
2024-11-08 10:16:10 -0300

    :star: returning to main.py first logic :star:

--- api/main.py
+++ api/main.py
@@ -5,7 +5,6 @@ from PyPDF2 import PdfReader
 import io
 from service.openai_service import generate_openai_summary
 from service.dalle_service import generate_dalle_image
-from utils.pdf_split import split_text
 import os
 from dotenv import load_dotenv
 
@@ -29,19 +28,10 @@ async def upload_pdf(file: UploadFile = File(...)):
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        parts = split_text(text)
-        partial_summaries = [generate_openai_summary(part) for part in parts]
-        combined_summary = " ".join(partial_summaries)
+        summary = generate_openai_summary(text[:8000])
+        image_url = generate_dalle_image(f"Create a minimalist, didactic and most real as possible image based on the following summary: {summary}")
 
-        final_summary = generate_openai_summary(combined_summary)
-        image_url = generate_dalle_image(f"Create a minimalist, didactic, and realistic image based on the following summary: {final_summary}")
-
-        return JSONResponse(
-            content={
-                "summary": final_summary, 
-                "dalle_image_url": image_url
-            }
-        )
+        return JSONResponse(content={"summary": summary, "dalle_image_url": image_url})
 
     except Exception as e:
-        return JSONResponse(content={ "error": str(e) }, status_code=500)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
